8c519497c588e1b9c0aeedad987f89270f2ed7f7
harshit <harshit@zeddlabz.com>
2023-08-14 15:07:50 +0530

    Done

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,14 @@ require("dotenv").config();
 const app = express();
 
 app.use(cookieParser());
-
-app.use(
-  "*",
-  cors({
-    origin: process.env.FRONT_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
+// app.use(
+//   "*",
+//   cors({
+//     origin: process.env.FRONT_URL,
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
